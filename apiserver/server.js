162162d2470f87cfe75ffec98aa505ac32ae8ed1
liwei <721755859@qq.com>
2019-06-12 14:37:25 +0800

    后台-注册登录

---
+++ apiserver/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// 引入路由中间件
+const userRouter = require('./routes/user');
+
+// 设置 req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/user', userRouter);
+
+app.listen(8080);
