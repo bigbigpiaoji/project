55740ff55edeec29476bb6a8b1dafe4d17a158a5
liwei <721755859@qq.com>
2019-06-13 15:18:46 +0800

    后端渲染的方式来实现分页

--- routes/page.js
+++ routes/page.js
@@ -1,27 +1,34 @@
 // 这个文件专门处理页面请求
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const router = express.Router();
 
 // 首页
 router.get('/', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, '../views/index.html'));
-  // render 的第二个参数，是带到模板上的数据。
-  res.render('index', {
-    name: '张三',
-    list: [
-      {
+  let pageNum = parseInt(req.query.pageNum) || 1;
+  let pageSize = parseInt(req.query.pageSize) || 10;
+
+  // 1. 先从数据库中来数据 直接去请求 http://loclahost:8080/api/student 的数据 普通请求 http.get
+  let url = `http://localhost:8080/api/student?pageNum=${pageNum}&pageSize=${pageSize}`;
+  http.get(url, (response) => {
+    let buf = '';
+    response.on('data', (chunk) => {
+      buf += chunk;
+    })
+    response.on('end', () => {
+      // console.log(buf);
+      let data = JSON.parse(buf);
+
+      res.render('index', {
         name: '张三',
-        age: 19,
-        grade: 'sz-1904'
-      },
-      {
-        name: '王五',
-        age: 18,
-        grade: 'sz-1903'
-      }
-    ]
-  });
+        list: data.data.list,
+        totalPage: data.data.totalPage,
+        pageNum: pageNum,
+        pageSize: pageSize
+      });
+    })
+  })
 })
 
 // 注册页
